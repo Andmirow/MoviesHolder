f7601b61e655af0f96eebee93cc2ffef6dc9e210
AndroidAndreich <heonito@mail.ru>
2022-11-28 22:37:11 +0300

    update filter form

--- app/src/main/java/com/example/moviesholder/presentation/FilterFragment.kt
+++ app/src/main/java/com/example/moviesholder/presentation/FilterFragment.kt
@@ -36,7 +36,7 @@ class FilterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.button.setOnClickListener {
+        binding.save.setOnClickListener {
             fragmentСontrol.closeFragment()
         }
 
