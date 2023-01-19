be91eb58785368e4c078484024a8aeaa5074e2aa
Andrey <heonito@mail.ru>
2023-01-19 21:27:03 +0300

    fix name params

--- app/src/main/java/com/example/moviesholder/presentation/recycler_view_tools/FilmListDiffItemCallBack.kt
+++ app/src/main/java/com/example/moviesholder/presentation/recycler_view_tools/FilmListDiffItemCallBack.kt
@@ -7,7 +7,7 @@ import com.example.moviesholder.domain.Film
 class FilmListDiffItemCallBack : DiffUtil.ItemCallback<Film>() {
 
     override fun areItemsTheSame(oldItem: Film, newItem: Film): Boolean {
-        return oldItem.idRetrofot == newItem.idRetrofot && oldItem.name == newItem.name
+        return oldItem.idRetrofit == newItem.idRetrofit && oldItem.name == newItem.name
     }
 
     override fun areContentsTheSame(oldItem: Film, newItem: Film): Boolean {
